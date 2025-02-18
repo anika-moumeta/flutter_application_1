b2a954527d056180f8a590ec4747d3120ae9e350
anika-moumeta <anikaafrin3344@gmail.com>
2025-02-18 09:32:39 +0600

    ani

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Lab05());
+}
+
+class Lab05 extends StatelessWidget {
+  const Lab05({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Basic UI - Flutter",
+      home: Scaffold(
+        appBar: ,))
