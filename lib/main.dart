717e32c7bd08e64e86d1c52e65df11d819b88340
anika-moumeta <anikaafrin3344@gmail.com>
2025-02-18 12:57:05 +0600

    LP2

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,83 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Lab05());
+  runApp(const MyApp());
 }
 
-class Lab05 extends StatelessWidget {
-  const Lab05({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Basic UI - Flutter",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Basic UI - Flutter"),
-          backgroundColor: Colors.blue[400],
-          centerTitle: true,
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Icon(
-              Icons.star,
-              color: Colors.yellow,
-              size: 50,
-            ),
-            Icon(
-              Icons.star,
-              color: Colors.yellow,
-              size: 50,
-            ),
-            Row(
-              children: [
-                Icon(
-                  Icons.star,
-                  color: Colors.yellow,
-                  size: 50,
-                ),
-                Icon(
-                  Icons.star,
-                  color: Colors.yellow,
-                  size: 50,
-                ),
-                Icon(
-                  Icons.star,
-                  color: Colors.yellow,
-                  size: 50,
-                ),
-              ],
-            ),
-          ],
+      title: 'Fancy Icon App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Shoe widget'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Text(
+                    '50\% OFF',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Icon(
+                    Icons.favorite
+                  )
+                ],
+              ),
+              const SizedBox(height: 20),
+
+              const Text(
+                'Nike Air Shoes',
+                style: TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 20),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Text(
+                    '\$45,000  ',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    '\$55,000',
+                    style: TextStyle(
+                      fontSize: 16,
+                      decoration: TextDecoration.lineThrough,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
